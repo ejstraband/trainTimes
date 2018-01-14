eff042ce3797f44c7ed83245efc864757715addc
Eric Traband <ejstraband@gmail.com>
2018-01-13 22:14:50 -0600

    initial commit

---
+++ assets/javascript/timeTable.js
@@ -0,0 +1,15 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCzHNerfuY3xgdFw3v8qXSMibV4agCoX_I",
+    authDomain: "timetables-54a3e.firebaseapp.com",
+    databaseURL: "https://timetables-54a3e.firebaseio.com",
+    projectId: "timetables-54a3e",
+    storageBucket: "",
+    messagingSenderId: "954313134492"
+  };
+  firebase.initializeApp(config);
+
+  database = firebase.database();
+
+  
